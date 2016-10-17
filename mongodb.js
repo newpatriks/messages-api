e13fbd419322965f2f54b6aef41bb2ef36174691
Jordi Llobet <jordi.llobet@me.com>
2016-10-16 22:15:19 -0400

    Update to mongodb config

--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,11 @@
 var mongoose            = require('mongoose');
 var SALT_WORK_FACTOR    = 10;
-var mongodbURL          = process.env.MONGODB_URI || 'mongodb://localhost/terete-api';
+var mongodbURL          = process.env.MONGODB_URI || 'mongodb://localhost:27017/terete-api';
 var mongodbOptions      = { };
 
-mongoose.connect(mongodbURL, mongodbOptions, function (err, res) {
-    if (err) {
-        console.log('Connection refused to ' + mongodbURL);
-        console.log(err);
-    } else {
-        console.log('...Mongo is ready');
-    }
+mongoose.connect(mongodbURL);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongoose > connection error:'));
+db.once('open', function() {
+    console.log('mongoose > connection successful!');
 });

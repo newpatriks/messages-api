170f5a2ef8fe7158ff904284f6568a2d6ce47dff
Jordi Llobet <jordi.llobet@me.com>
2016-10-16 22:21:01 -0400

    Print mongodb when mongo error connection happens

--- mongodb.js
+++ mongodb.js
@@ -1,11 +1,10 @@
 var mongoose            = require('mongoose');
 var SALT_WORK_FACTOR    = 10;
 var mongodbURL          = process.env.MONGODB_URI || 'mongodb://localhost:27017/terete-api';
-var mongodbOptions      = { };
 
 mongoose.connect(mongodbURL);
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'mongoose > connection error:'));
+db.on('error', console.error.bind(console, 'mongoose > connection error: ', mongodbURL));
 db.once('open', function() {
     console.log('mongoose > connection successful!');
 });

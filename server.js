4b0478091794aed346b9270475d6819e969f018e
Jordi Llobet <jordi.llobet@me.com>
2016-10-19 12:15:45 -0400

    Adds domain to CORS

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ io.on('connection', function(socket) {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.header("Access-Control-Allow-Origin", "http://annafuste.com/terete/");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 

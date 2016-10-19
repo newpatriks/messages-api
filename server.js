605d208144721159abdd2006b79940f4d6f7cf1d
Jordi Llobet <jordi.llobet@me.com>
2016-10-19 12:17:50 -0400

    Sets credentials header to false

--- server.js
+++ server.js
@@ -35,8 +35,9 @@ io.on('connection', function(socket) {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials: false');
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });

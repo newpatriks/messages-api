cb8ce7b0f46fb75fd67d052b13bd5bdb582ab00a
Jordi Llobet <jordi.llobet@me.com>
2016-10-19 12:11:46 -0400

    Added CORS configuration

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ io.on('connection', function(socket) {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Origin", "http://annafuste.com/terete/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
   next();
 });
 
